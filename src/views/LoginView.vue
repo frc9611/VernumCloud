571ed8cf1f8c676029bb024ea48141863a0cf22b
Guilherme077 <gui.pilz.2007@gmail.com>
2025-12-09 18:18:00 -0300

    Add login form and HTTP service with axios

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,29 @@
 <template>
   <h2>Login</h2>
+
+  <form @submit.prevent="login">
+    <input type="text" placeholder="username" v-model="user.username">
+    <input type="password" placeholder="*****" v-model="user.password">
+    <button type="submit">Login</button>
+  </form>
 </template>
 <script setup>
+  import http from '@/services/http.js';
+  import { reactive } from 'vue';
+
+  const user = reactive({
+    username:'',
+    password:''
+  });
+
+  async function login(){
+    try{
+      const {data} = await http.post('/login', user);
+      console.log(data);
+    }catch(error){
+      console.log(error?.response?.data);
+    }
+  }
 
 </script>
 <style lang="scss" scoped>
