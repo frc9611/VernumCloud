8bca3f7d75571c8a87e48c21aa1b3198bbbfc5d6
Guilherme077 <gui.pilz.2007@gmail.com>
2025-12-09 16:22:08 -0300

    Add login view and update navigation structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <ul>
+    <li><Router-link :to="{name:'login'}">Login</Router-link></li>
+    <li><Router-link :to="{name:'home'}">Home</Router-link></li>
+    <li><Router-link :to="{name:'about'}">Sobre o Vernum Cloud</Router-link></li>
+  </ul>
+<router-view/>
 </template>
+<script setup>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+</script>
+<style lang="scss" scoped>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
