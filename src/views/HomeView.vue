cd59a0089c912446555e3052eceb006bfa3dec31
Guilherme077 <gui.pilz.2007@gmail.com>
2026-01-15 14:24:31 -0300

    Display announcements on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Olá {{ auth.getName }}</h1>
-    <span class="information">Sistema em testes</span> 
+    <span v-for="announcement in data" :key="announcement.announcementId" class="information"><strong> {{ announcement.title }} </strong> <br> {{ announcement.content }}</span> 
 
     <ul class="main-menu">
       <li><Router-link :to="{ name: 'home' }" class="menu-button">
@@ -24,8 +24,29 @@
 </template>
 
 <script setup>
-  import {authStore} from '@/store/auth.js'
+  import {authStore} from '@/store/auth.js';
+import { onMounted, ref } from 'vue';
+import http from '@/services/http.js';
+import { useToast } from "vue-toastification"
+
+const toast = useToast();
   const auth = authStore();
+const data = ref([]);
+
+  onMounted(async () => {
+    try{
+      const response = await http.get('/allAnnouncements', {
+        headers: {
+        Authorization: `Bearer ${auth.getToken}`
+        }
+      });
+      data.value = response.data;
+    }catch (err){
+      toast.error("Erro ao carregar avisos");
+      console.log(err?.response?.data);
+      console.log(err.message)
+    }
+  });
 </script>
 
 <style>
